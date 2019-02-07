92ad3efcd2f784476330cae00dfc112a3de63a42
Daniel Luz <danielluz1995@gmail.com>
2019-02-07 17:10:09 -0200

    Remove code related to video and preview.

--- record_to_slack/views.py
+++ record_to_slack/views.py
@@ -7,9 +7,11 @@ from flask import render_template, request, redirect, url_for, session
 from requests_oauthlib import OAuth2Session
 from flask_login import login_required, current_user, login_user, logout_user
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.filter_by(user_id=user_id).first()
+
 
 def get_slack_auth(state=None, token=None, redirect_uri=None):
     if token:
@@ -23,6 +25,7 @@ def get_slack_auth(state=None, token=None, redirect_uri=None):
     )
     return oauth
 
+
 @app.route("/")
 @login_required
 def home():
@@ -30,19 +33,22 @@ def home():
     if slack_bot is not None:
         return redirect(url_for('record'))
     else:
-        return render_template('home.html', redirect_uri = os.getenv('SLACK_REDIRECT_ADD_URI'))
+        return render_template('home.html', redirect_uri=os.getenv('SLACK_REDIRECT_ADD_URI'))
+
 
 @app.route("/login")
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('record'))
-    return render_template('login.html', redirect_uri = os.getenv('SLACK_REDIRECT_URI'))
+    return render_template('login.html', redirect_uri=os.getenv('SLACK_REDIRECT_URI'))
+
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route("/slack/add/redirect")
 def oauth_add_redirect():
@@ -65,6 +71,7 @@ def oauth_add_redirect():
         db.session.add(slack_bot)
         db.session.commit()
     return redirect(url_for('home'))
+
 
 @app.route("/slack/login/redirect")
 def oauth_login_redirect():
@@ -92,10 +99,12 @@ def oauth_login_redirect():
         login_user(user, remember=True)
         return redirect(url_for('home'))
 
+
 @app.route("/record", methods=['GET'])
 @login_required
 def record():
     return render_template('record.html')
+
 
 @app.route("/recording", methods=['POST'])
 @login_required
@@ -106,15 +115,15 @@ def post_recording():
         "filename": "{}.{}".format(DT.datetime.now().strftime("%c"), 'mp3'),
         "channels": ["#general"],
         "token": token,
-        "initial_comment": "New recording by <@{}>".format(current_user.user_id)
+        "initial_comment": "Recorded by: <@{}>".format(current_user.user_id)
     }
 
     oauth = get_slack_auth()
-    r = oauth.post(
+    oauth.post(
         url,
         params=payload,
-        files= {
-            'file' : request.files.get('recording')
+        files={
+            'file': request.files.get('recording')
         },
     )
     return "200 OK"
